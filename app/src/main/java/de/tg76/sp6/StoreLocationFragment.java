5eefa5050ffa016e759de89c76b931f0faa2bf02
Toto <graebi@gmail.com>
2015-11-22 00:26:44 +0000

    TAB 2 - StoreLocationFragement - Speicherung von Latitude/Longitude funktioniert -> pushed to DB

--- app/src/main/java/de/tg76/sp6/StoreLocationFragment.java
+++ app/src/main/java/de/tg76/sp6/StoreLocationFragment.java
@@ -1,6 +1,7 @@
 package de.tg76.sp6;
 
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -19,6 +20,18 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import java.util.ArrayList;
+
 /**
  *
  * Created by adm_toto on 16/11/2015.
@@ -26,6 +39,14 @@ import com.google.android.gms.location.LocationServices;
 public class StoreLocationFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener{
 
+    //Show loading bar when server request is executed
+    //private final ProgressDialog progressDialog;
+
+
+
+    //Connection time in mill sec before disconnect
+    private static final int CONNECTION_TIME = 1000*15;
+    private static final String SERVER_ADDRESS = "http://ec2-52-17-188-91.eu-west-1.compute.amazonaws.com/";
    // -- troubleshooting, delete
     // LogCat tag
     // private static final String TAG = MainActivity.class.getSimpleName();
@@ -34,12 +55,16 @@ public class StoreLocationFragment extends Fragment implements GoogleApiClient.C
 
     private Location mLastLocation;
 
+    // Response
+    String responseServer,description;
+    double latitude=0,longitude=0 ;
+    int customer_idfk;
+    TextView txt;
+
 //-----------------------------------
   //  private EditText etName;
   //  private EditText etEmail;
-    private EditText etUsername, etUserID;
-    private UserLocalStore userLocalStore;
-
+    private EditText etUsername, etUserID;//loeschen
 
 
 
@@ -59,6 +84,8 @@ public class StoreLocationFragment extends Fragment implements GoogleApiClient.C
     // UI elements
     private TextView lblLocation;
     private Button btnShowLocation;
+    private EditText edDescription;
+    private UserLocalStore userLocalStore;
 
 
     @Nullable
@@ -75,10 +102,12 @@ public class StoreLocationFragment extends Fragment implements GoogleApiClient.C
 
         lblLocation = (TextView) getActivity().findViewById(R.id.lblLocation);
         btnShowLocation = (Button) getActivity().findViewById(R.id.btnShowLocation);
+        edDescription = (EditText)getActivity().findViewById(R.id.edDescription);
+
         Log.d("Testing", "onActivityCreated - btnShowLocation called");
 
         //-----------------------------
-        //Assign ID to variable
+        //Assign ID to variable - loeschen
        // etName = (EditText) getActivity().findViewById(R.id.etName);
        // etEmail = (EditText) getActivity().findViewById(R.id.etEmail);
         etUsername = (EditText) getActivity().findViewById(R.id.etUsername);
@@ -105,6 +134,8 @@ public class StoreLocationFragment extends Fragment implements GoogleApiClient.C
                 displayLocation();
                 Log.d("Testing", "StoreLocationFragment call displayUserDetails function");
                 displayUserDetails();
+                AsyncT asyncT = new AsyncT();
+                asyncT.execute();
             }
         });
 
@@ -115,12 +146,13 @@ public class StoreLocationFragment extends Fragment implements GoogleApiClient.C
      * */
     private void displayLocation() {
 
+        description = edDescription.getText().toString();
         mLastLocation = LocationServices.FusedLocationApi
                 .getLastLocation(mGoogleApiClient);
 
         if (mLastLocation != null) {
-            double latitude = mLastLocation.getLatitude();
-            double longitude = mLastLocation.getLongitude();
+             latitude = mLastLocation.getLatitude();
+             longitude = mLastLocation.getLongitude();
 
             //Write co-ordination on screen
             lblLocation.setText(latitude + ", " + longitude);
@@ -202,14 +234,64 @@ public class StoreLocationFragment extends Fragment implements GoogleApiClient.C
 
     //Function to display user details on activity_main
     private void displayUserDetails(){
-        Log.d("User", "displayUserDetails function");
+        Log.d("Testing", "StroeLocationFragnemt displayUserDetails function");
         User user = userLocalStore.getLoggedInUser();
 
         etUsername.setText(user.username);
         etUserID.setText(user.customer_id +"");
+        Log.d("Testing", "StroeLocationFragnemt user.customer_id");
+        customer_idfk = user.customer_id;
 
     }//End displayUserDetails
 
 
+
+    /* Inner class to get response */
+    public class AsyncT extends AsyncTask<Void, Void, Void> {
+
+
+        String nlatitude = String.valueOf(latitude);
+        String nlongitude = String.valueOf(longitude);
+        String ncustomer_idfk = String.valueOf(customer_idfk) ;
+
+            @Override
+            //Run in the background when StoreUserDataAsyncTask starts - Accessing the server
+            protected Void doInBackground(Void... params) {
+                //Data which are send to server
+                Log.d("Testing", "StroeLocationFragnemt doInBackground");
+                ArrayList<NameValuePair> dataToSend = new ArrayList<>();
+                dataToSend.add(new BasicNameValuePair("longitude",nlatitude ));
+                dataToSend.add(new BasicNameValuePair("latitude", nlongitude));
+                dataToSend.add(new BasicNameValuePair("description", description));
+                dataToSend.add(new BasicNameValuePair("customer_idfk", ncustomer_idfk));
+
+                HttpParams httpRequestParams = new BasicHttpParams();
+                //Time to wait before the post is executed
+                HttpConnectionParams.setConnectionTimeout(httpRequestParams, CONNECTION_TIME);
+                //Time to wait to receive anything from server
+                HttpConnectionParams.setSoTimeout(httpRequestParams, CONNECTION_TIME);
+
+                //Create client to establish HTTP connection + and make request to server
+                HttpClient client = new DefaultHttpClient(httpRequestParams);
+                HttpPost post = new HttpPost(SERVER_ADDRESS + "StoreCoordinates.php");
+
+                try{
+                    post.setEntity(new UrlEncodedFormEntity(dataToSend));
+                    client.execute(post);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                return null;
+            }
+            @Override
+            //When AsyncTask is finish
+            protected void onPostExecute(Void aVoid) {
+                //Stop progress bar displayed when AsyncTask is finish
+             //   progressDialog.dismiss();
+                //Inform activity which started process that process is finished
+              //  userCallback.done(null);
+                super.onPostExecute(aVoid);
+            }
+        }
 
 }//End main
